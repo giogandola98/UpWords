cd81b29bf23a2f5175300cd328ef56d376c15d33
giogandola98 <giogandola@gmail.com>
2018-09-13 19:54:15 +0200

    Revert "some fix"
    This reverts commit cf1f7a21f1fa5ef32d8cd51736dc6045ff68eeb6.

--- UpWords/giocatore.cpp
+++ UpWords/giocatore.cpp
@@ -1,19 +1,14 @@
 #include "giocatore.h"
 #include "sacchetto.h"
-void giocatore::reset()
-{
-    points=0;
-    for(short unsigned int i=0;i<MAX_LETTERS_HAND;i++)
-        letters[i]=' ';
-}
 giocatore::giocatore()
 {
-  giocatore::reset();
+
 }
 /*
  *giocatore::giocatore(sacchetto &s)
  * {
- * giocatore::reset();
+ * for(short unsigned int i=0;i<MAX_LETTERS_HAND:i++)
+ *   letters[i]=" ";
  * short unsigned int i=0;
  * while(!giocatore::hand_full())&&(!s.this_is_empity())&&(i<MAX_LETTERS_HAND))
  * {
--- UpWords/giocatore.h
+++ UpWords/giocatore.h
@@ -18,7 +18,6 @@ public:
 private:
     unsigned int points;
     char letters[MAX_LETTERS_HAND];
-    void reset();
 
 };
 
