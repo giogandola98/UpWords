dd6bbafa2aa357051d6da697e0eda484a0918832
giogandola98 <giogandola@gmail.com>
2018-10-04 12:20:16 +0200

    fix inserimento giocatore
    da finere la croce

--- build-UpWords-Desktop_Qt_5_11_1_MinGW_32bit-Debug/debug/moc_mainwindow.cpp
+++ build-UpWords-Desktop_Qt_5_11_1_MinGW_32bit-Debug/debug/moc_mainwindow.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[15];
-    char stringdata0[270];
+    QByteArrayData data[16];
+    char stringdata0[289];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -45,7 +45,8 @@ QT_MOC_LITERAL(10, 146, 21), // "on_cambia_btn_clicked"
 QT_MOC_LITERAL(11, 168, 23), // "on_conferma_btn_clicked"
 QT_MOC_LITERAL(12, 192, 25), // "on_passaturno_btn_clicked"
 QT_MOC_LITERAL(13, 218, 25), // "on_conferma_btn_3_clicked"
-QT_MOC_LITERAL(14, 244, 25) // "on_conferma_btn_2_clicked"
+QT_MOC_LITERAL(14, 244, 25), // "on_conferma_btn_2_clicked"
+QT_MOC_LITERAL(15, 270, 18) // "on_end_btn_clicked"
 
     },
     "MainWindow\0on_pushButton_7_clicked\0\0"
@@ -56,7 +57,7 @@ QT_MOC_LITERAL(14, 244, 25) // "on_conferma_btn_2_clicked"
     "on_conferma_btn_clicked\0"
     "on_passaturno_btn_clicked\0"
     "on_conferma_btn_3_clicked\0"
-    "on_conferma_btn_2_clicked"
+    "on_conferma_btn_2_clicked\0on_end_btn_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -66,7 +67,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      11,   14, // methods
+      12,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -74,17 +75,18 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   69,    2, 0x08 /* Private */,
-       3,    2,   70,    2, 0x08 /* Private */,
-       6,    0,   75,    2, 0x08 /* Private */,
-       7,    0,   76,    2, 0x08 /* Private */,
-       8,    2,   77,    2, 0x08 /* Private */,
-       9,    0,   82,    2, 0x08 /* Private */,
-      10,    0,   83,    2, 0x08 /* Private */,
-      11,    0,   84,    2, 0x08 /* Private */,
-      12,    0,   85,    2, 0x08 /* Private */,
-      13,    0,   86,    2, 0x08 /* Private */,
-      14,    0,   87,    2, 0x08 /* Private */,
+       1,    0,   74,    2, 0x08 /* Private */,
+       3,    2,   75,    2, 0x08 /* Private */,
+       6,    0,   80,    2, 0x08 /* Private */,
+       7,    0,   81,    2, 0x08 /* Private */,
+       8,    2,   82,    2, 0x08 /* Private */,
+       9,    0,   87,    2, 0x08 /* Private */,
+      10,    0,   88,    2, 0x08 /* Private */,
+      11,    0,   89,    2, 0x08 /* Private */,
+      12,    0,   90,    2, 0x08 /* Private */,
+      13,    0,   91,    2, 0x08 /* Private */,
+      14,    0,   92,    2, 0x08 /* Private */,
+      15,    0,   93,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -92,6 +94,7 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void, QMetaType::Int, QMetaType::Int,    4,    5,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -119,6 +122,7 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 8: _t->on_passaturno_btn_clicked(); break;
         case 9: _t->on_conferma_btn_3_clicked(); break;
         case 10: _t->on_conferma_btn_2_clicked(); break;
+        case 11: _t->on_end_btn_clicked(); break;
         default: ;
         }
     }
@@ -149,13 +153,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 11)
+        if (_id < 12)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 11;
+        _id -= 12;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 11)
+        if (_id < 12)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 11;
+        _id -= 12;
     }
     return _id;
 }
